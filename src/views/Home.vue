3afb70a08af918de91a72bdc99d38fd1a71464ef
Mikhail Nekrasov <57632295+M1kentosh@users.noreply.github.com>
2020-02-13 14:21:11 +0300

    init commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <issues-list/>
+    <pagination/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import IssuesList from '@/components/IssuesList.vue';
+import Pagination from '@/components/Pagination.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    IssuesList,
+    Pagination,
   },
 };
 </script>
