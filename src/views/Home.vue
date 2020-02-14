d61e97f1e3d04d0e6e16631295a791147cfbb148
Mikhail Nekrasov <57632295+M1kentosh@users.noreply.github.com>
2020-02-14 09:50:42 +0300

    delete unused import

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
     <issues-list/>
-    <pagination/>
   </div>
 </template>
 
 <script>
 import IssuesList from '@/components/IssuesList.vue';
-import Pagination from '@/components/Pagination.vue';
 
 export default {
   name: 'Home',
   components: {
     IssuesList,
-    Pagination,
   },
 };
 </script>
