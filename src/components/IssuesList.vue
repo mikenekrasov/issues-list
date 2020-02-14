20f2242d487c7dc083a655ed2726fcceaaa10fc9
Mikhail Nekrasov <57632295+M1kentosh@users.noreply.github.com>
2020-02-14 09:41:36 +0300

    pagination added

--- src/components/IssuesList.vue
+++ src/components/IssuesList.vue
@@ -6,6 +6,12 @@
     <ul class="issues-list">
       <issue-item v-for="(issue, i) in issues" :key="i" :issue="issue"/>
     </ul>
+    <button @click="fetchData(url.url)"
+            v-for="(url, i) in urls" :key="i"
+            class="btn pagination-btn"
+    >
+      {{ url.title }}
+    </button>
   </div>
 </template>
 
@@ -20,19 +26,36 @@ export default {
   data() {
     return {
       issues: [],
+      urls: [],
     };
   },
   mounted() {
     this.fetchData();
   },
   methods: {
-    async fetchData() {
-      const url = 'https://api.github.com/repos/vuejs/vue/issues?state=open';
-
+    async fetchData(url = 'https://api.github.com/repos/vuejs/vue/issues') {
       try {
         const response = await fetch(url);
 
         if (response.ok) {
+          // pagination
+
+          const link = response.headers.get('link');
+          const links = link.split(',');
+
+          this.urls = links.map((item) => {
+            const formatUrl = item.split(';')[0]
+              .replace('>', '')
+              .replace('<', '');
+            const formatTitle = item.split(';')[1]
+              .replace('rel="', '')
+              .replace('"', '');
+            return {
+              url: formatUrl,
+              title: formatTitle,
+            };
+          });
+
           this.issues = await response.json();
         }
       } catch (e) {
